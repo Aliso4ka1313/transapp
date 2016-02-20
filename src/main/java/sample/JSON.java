52d8ad044e819dbb3ea592ba5afa8629fbfaa86b
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2016-02-20 14:04:41 +0200

    pom added

--- src/main/java/sample/JSON.java
+++ src/main/java/sample/JSON.java
@@ -3,10 +3,10 @@ package sample;
 import javax.management.Query;
 
 /**
- * Created by Администратор on 16.10.2015.
+ * Created by пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ on 16.10.2015.
  */
 public class JSON {
-    //public Query query;
+    public Query query;
     public String code;
     public String lang;
     public String[] text;
