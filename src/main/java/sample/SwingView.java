0e6a8895e34ff6a776cbf45c3886e90c0a9e5f7d
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2016-02-21 10:33:55 +0200

    Swing added

---
+++ src/main/java/sample/SwingView.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javax.swing.*;
+
+
+public class SwingView  extends JFrame{
+
+    public SwingView(){
+        setSize(500,500);
+        setTitle("Transapp");
+        //this.setContentPane(jPanel);
+        JPanel jPanel = new JPanel();
+        JButton translateButton = new JButton("Translate");
+        JTextArea originText = new JTextArea("Text to translate");
+        jPanel.add(translateButton);
+        jPanel.add(originText);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingView swingView = new SwingView();
+
+
+    }
+}
