5e88e0dc6054c0faafd56d95a3382286bcaa2344
aliso4ka <Vasilevskaya9090@mail.ru>
2016-03-05 16:39:03 +0200

    fXview done

--- src/main/java/sample/FXView.java
+++ src/main/java/sample/FXView.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class FXView extends Application{
@@ -12,11 +13,12 @@ public class FXView extends Application{
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        String fxmlFile = "./resources/hello.fxml";
+        String fxmlFile = "/hello.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-        primaryStage.setTitle("JavaFX and Maven");
+        primaryStage.setTitle("Translate Application");
         primaryStage.setScene(new Scene(root));
+
         primaryStage.show();
     }
 }
--- src/main/java/sample/MainController.java
+++ src/main/java/sample/MainController.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class MainController {
+    @FXML private TextArea originText;
+    @FXML private TextArea translatedText;
+
+    @FXML protected void handleTranslateButtonAction (ActionEvent event){
+        String textToTranslate = originText.getText();
+        textToTranslate = textToTranslate.replace("\r\n","%0D%0A").replace("\n", "%0D%0A").replace("\r", "%0D%0A");
+        String translated = TranslateUtil.translate(textToTranslate);
+        translatedText.setText(translated);
+    }
+
+
 }
